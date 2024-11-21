dc24dbdfe5b378b976fa176bf2cf6457f26a150b
v-shub <victoriya.shubina.mex@gmail.com>
2024-11-21 15:52:14 +0300

    add read + delete + part add

--- src/ConsoleAppUsers/ConsoleAppUsers/Program.cs
+++ src/ConsoleAppUsers/ConsoleAppUsers/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,16 @@ namespace ConsoleAppUsers
 
             string connString = "Server=.;Database=FamilyLife;Trusted_Connection=True;";
             SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            string wrongInputMessage = "Неверный формат, повторите еще раз";
+            string sqlQuery;
+            SqlCommand cmd;
+            int id;
+            string userName;
+            string password;
+            DateTime birthDate;
+            int childrenCount;
+            string extraInfo;
 
             while (true)
             {
@@ -23,13 +35,50 @@ namespace ConsoleAppUsers
                 switch (userChoiceNum)
                 {
                     case "1":
-                        Console.WriteLine("Добавление пользователя:\nВведите следующие данные через запятую \",\"\nFirstname,Username,Password,Age");
+                        sqlQuery = "select * from Users where Deleted_at is null";
+                        cmd = new SqlCommand(sqlQuery, conn);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            id = reader.GetInt32(0);
+                            userName = reader.GetString(1);
+                            birthDate = reader.GetDateTime(3);
+                            childrenCount= reader.GetInt32(4);
+                            Console.WriteLine($"{ id }\t{ userName }\t{ birthDate }\t{ childrenCount}");
+                        }
+                        reader.Close();
                         break;
                     case "2":
+                        Console.WriteLine("Добавление пользователя:\nВведите следующие данные через запятую \",\"\nName,Password,Birthday_date,Children_count,Extra_info");
+                        while (true)
+                        {
+                            string[] stringAddInput = Console.ReadLine().Split(',');
+                            if (stringAddInput.Length != 5)
+                            {
+                                Console.WriteLine(wrongInputMessage);
+                                continue;
+                            }
+                            userName = stringAddInput[0];
+                            password = stringAddInput[1];
+                            extraInfo = stringAddInput[4];
+                            bool isChCountCorrect = Int32.TryParse(stringAddInput[3], out childrenCount);
+                            bool isBirthDateCorrect= DateTime.TryParse(stringAddInput[2], out birthDate);
+                            if (isBirthDateCorrect & isChCountCorrect & userName.Length < 51 & password.Length < 31 & extraInfo.Length < 4001)
+                                break;
+                            else
+                                Console.WriteLine(wrongInputMessage);
+                        }
+
                         break;
                     case "3":
                         break;
                     case "4":
+                        Console.WriteLine("Удаление пользователя:\nВведите ID пользователя, которого хотите удалить");
+                        bool isIDCorrect = Int32.TryParse(Console.ReadLine(), out id);
+                        sqlQuery = $"UPDATE Users SET Deleted_at = getdate() WHERE ID = {id}";
+                        cmd = new SqlCommand(sqlQuery, conn);
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        Console.WriteLine(rowsAffected + " user(s) deleted");
                         break;
                     case "5":
                         break;
@@ -37,6 +86,8 @@ namespace ConsoleAppUsers
                         Console.WriteLine("Ваш выбор не соответствует ни одному варианту. Попробуйте ещё раз");
                         break;
                 }
+                Console.WriteLine("Нажмите enter чтобы вернуться");
+                Console.ReadLine();
                 Console.Clear();
             }
         }
