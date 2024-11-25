523f786cb6773e8ae40f04907c0b2f3c54528443
v-shub <victoriya.shubina.mex@gmail.com>
2024-11-25 11:58:19 +0300

    add update

--- src/WpfAppPosts/WpfAppPosts/MainWindow.xaml.cs
+++ src/WpfAppPosts/WpfAppPosts/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
 
 namespace WpfAppPosts
 {
@@ -20,9 +12,69 @@ namespace WpfAppPosts
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connectionString;
+        SqlDataAdapter adapter;
+        DataTable postsTable;
         public MainWindow()
         {
             InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            string sql = "SELECT * FROM Posts";
+            postsTable = new DataTable();
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                SqlCommand command = new SqlCommand(sql, connection);
+                adapter = new SqlDataAdapter(command);
+
+                // установка команды на добавление для вызова хранимой процедуры
+                adapter.InsertCommand = new SqlCommand("sp_InsertPost", connection);
+                adapter.InsertCommand.CommandType = CommandType.StoredProcedure;
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Header", SqlDbType.NVarChar, 200, "Header"));
+                adapter.InsertCommand.Parameters.Add(new SqlParameter("@Post_content", SqlDbType.NVarChar, 4000, "Post_content"));
+                adapter.InsertCommand.Parameters.Add("@Creator_ID", SqlDbType.Int, 0, "Creator_ID");
+
+                connection.Open();
+                adapter.Fill(postsTable);
+                postsGrid.ItemsSource = postsTable.DefaultView;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+            }
+        }
+        private void UpdateDB()
+        {
+            SqlCommandBuilder comandbuilder = new SqlCommandBuilder(adapter);
+            adapter.Update(postsTable);
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateDB();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (postsGrid.SelectedItems != null)
+            {
+                for (int i = 0; i < postsGrid.SelectedItems.Count; i++)
+                {
+                    DataRowView datarowView = postsGrid.SelectedItems[i] as DataRowView;
+                    if (datarowView != null)
+                    {
+                        DataRow dataRow = (DataRow)datarowView.Row;
+                        dataRow.Delete();
+                    }
+                }
+            }
+            UpdateDB();
         }
     }
 }
